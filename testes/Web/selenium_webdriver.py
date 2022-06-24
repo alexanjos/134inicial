dd2d9912e0daed1da35db239bb80b0b6b6677f7d
alexanjos <alexalvesdosanjos09@gmail.com>
2022-06-23 22:04:27 -0300

    Aula 21 em 23/06/2022

--- testes/Web/selenium_webdriver.py
+++ testes/Web/selenium_webdriver.py
@@ -1,10 +1,15 @@
 # Configura
 # Bibliotecas / Imports
-from selenium import webdriver
+import time
 
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 # Dados de Entrada
+
+
 origem = 'São Paolo'
 destino = 'New York'
 primeiro_nome = 'Juca'
@@ -25,6 +30,8 @@ class Testes:
         # instanciar a biblioteca / motor / engine
         # informar onde está o WebDriver
         self.driver = webdriver.Chrome()
+        # Espera até 15 segundos por qualquer elemento
+        self.driver.implicitly_wait(15)
 
     # Fim
     def teardown_method(self):
@@ -36,9 +43,26 @@ class Testes:
         # e2e / end to end / ponta a ponta
         # Página Inicial (Home)
         # Executa / Valida
+        # Abrir o browser no endereço
+        self.driver.get('https://www.blazedemo.com')
+        lista = self.driver.find_element(By.NAME, "fromPort")
+        lista.click()
+        # selecionar a cidade de origem
+        lista.find_element(By.XPATH, f'//option[ .= "{origem}"]').click()
+        # clicar na lista de cidades de destino
+        lista = self.driver.find_element(By.NAME, 'toPort')
+        lista.click()
+        # Selecionar a cidade de destino na lista
+        lista.find_element(By.XPATH, f'//option[ .= "{destino}"]').click()
+        # Clicar no botão de procurar voos
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
+
 
         # Página Lista de Passagens
         # Executa / Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h3').text == titulo_passagens_esperados
+        self.driver.find_element(By.CSS_SELECTOR, 'tr:nth-child(1) .btn').click()
+        time.sleep(2)
 
         # Página de Compra
         # Executa / Valida
