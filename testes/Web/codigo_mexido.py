56066e93164525c2989aafbfd402783cb36db687
alexanjos <alexalvesdosanjos09@gmail.com>
2022-06-28 22:07:16 -0300

    Aula 22 em 28/06/2022

--- testes/Web/codigo_mexido.py
+++ testes/Web/codigo_mexido.py
@@ -1,7 +1,7 @@
 # Configura
 # Bibliotecas / Imports
-import time
 
+import pytest
 from selenium import webdriver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,14 +10,15 @@ from selenium.webdriver.common.by import By
 # Dados de Entrada
 
 
-origem = 'São Paolo'
-destino = 'New York'
+#origem = 'São Paolo'
+#destino = 'New York'
 primeiro_nome = 'Juca'
 bandeira = 'American Express'
 lembrar_de_mim = True
 
+
 # Resultados Esperados
-titulo_passagens_esperados = 'Flights from São Paolo to New York:'
+# titulo_passagens_esperados = 'Flights from São Paolo to New York:'
 mensagem_de_agradecimento_esperada = 'Thank you for your purchase today!'
 preco_passagem_esperado = '555 USD'
 
@@ -39,7 +40,14 @@ class Testes:
         self.driver.quit()
 
     # Meio
-    def testar_comprar_passagem(self):
+    # lista para uso como massa de teste
+    lista_de_valores = [
+        ('São Paolo', 'New York'),
+        ('Boston', 'New York'),
+        ('San Diego', 'New York')
+    ]
+    @pytest.mark.parametrize('origem, destino', lista_de_valores)
+    def testar_comprar_passagem(self, origem, destino):
         # e2e / end to end / ponta a ponta
         # Página Inicial (Home)
         # Executa / Valida
@@ -60,13 +68,33 @@ class Testes:
 
         # Página Lista de Passagens
         # Executa / Valida
-        assert self.driver.find_element(By.TAG_NAME, 'h3').text == titulo_passagens_esperados
+        assert self.driver.find_element(By.TAG_NAME, 'h3').text == f'Flights from {origem} to {destino}:'
         self.driver.find_element(By.CSS_SELECTOR, 'tr:nth-child(1) .btn').click()
 
         # Página de Compra
         # Executa / Valida
+        # Limpar o campo do nome para evitar problemas ao digitar
+        self.driver.find_element(By.ID, 'inputName').clear()
+        # Prenche o nome do comprador
+        self.driver.find_element(By.ID, 'inputName').send_keys(primeiro_nome)
+
+        # Seleciona a bandeira do cartão
+        lista = self.driver.find_element(By.ID, 'cardType')
+        lista.click()
+        lista.find_element(By.XPATH, f'//option[ .= "{bandeira}"]').click()
+
+        # Marca o checkbox para ser lembrado
+        self.driver.find_element(By.ID, 'rememberMe').click()
+
+        # Aperta o botão Purchase Flight
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
+
+
 
         # Página de Obrigado
-        # Executa / Valida
+        # Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h1').text == mensagem_de_agradecimento_esperada
+        assert self.driver.find_element(By.CSS_SELECTOR, 'tr:nth-child(3) > td:nth-child(2)').text == \
+               preco_passagem_esperado
 
     # Valida
